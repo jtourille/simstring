8b5b9b6f90d4ab89f337abbd132d33f77a66cf15
Julien Tourille <julien.tourille@gmail.com>
2021-01-19 11:39:17 +0100

    dice implementation correction + min and max feature size computation refactoring

--- simstring/measure/dice.py
+++ simstring/measure/dice.py
@@ -1,15 +1,16 @@
 import math
 from .base import BaseMeasure
 
+
 class DiceMeasure(BaseMeasure):
     def min_feature_size(self, query_size, alpha):
-        return int(math.ceil(alpha * 1.0 / (2 - alpha) * query_size))
+        return int(math.ceil((alpha / (2 - alpha)) * query_size))
 
     def max_feature_size(self, query_size, alpha):
-        return int(math.floor((2 - alpha) * query_size * 1.0 / alpha))
+        return int(math.floor(((2.0 - alpha) / alpha) * query_size))
 
     def minimum_common_feature_count(self, query_size, y_size, alpha):
-        return int(math.ceil(0.5 * alpha * query_size * y_size))
+        return int(math.ceil(0.5 * alpha * (query_size + y_size)))
 
     def similarity(self, X, Y):
         return len(set(X) & set(Y)) * 2.0 / (len(set(X)) + len(set(Y)))
