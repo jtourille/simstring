f767a2455f71a5b71a1984da9693c288359c1db7
Julien Tourille <julien.tourille@gmail.com>
2021-01-19 11:41:22 +0100

    reformatting with black

--- simstring/measure/cosine.py
+++ simstring/measure/cosine.py
@@ -1,5 +1,7 @@
 import math
+
 from .base import BaseMeasure
+
 
 class CosineMeasure(BaseMeasure):
     def min_feature_size(self, query_size, alpha):
@@ -12,4 +14,6 @@ class CosineMeasure(BaseMeasure):
         return int(math.ceil(alpha * math.sqrt(query_size * y_size)))
 
     def similarity(self, X, Y):
-        return len(set(X) & set(Y)) * 1.0 / math.sqrt(len(set(X)) * len(set(Y)))
+        return (
+            len(set(X) & set(Y)) * 1.0 / math.sqrt(len(set(X)) * len(set(Y)))
+        )
