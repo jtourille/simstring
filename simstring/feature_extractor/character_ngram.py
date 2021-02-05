eb1a1a7f1bd41a9cd74c5c42a8a527dbe150650b
Julien Tourille <julien.tourille@gmail.com>
2021-02-05 15:51:05 +0100

    bug correction: implement the official simstring algorithm

--- simstring/feature_extractor/character_ngram.py
+++ simstring/feature_extractor/character_ngram.py
@@ -1,10 +1,32 @@
 from .base import BaseFeatureExtractor
+from collections import Counter
 
-SENTINAL_CHAR = " "  # non breaking space
+SENTINAL_CHAR = "\xa0"  # non breaking space
+
 
 class CharacterNgramFeatureExtractor(BaseFeatureExtractor):
-    def __init__(self, n=2):
+    def __init__(self, n: int = 3, be: bool = False):
         self.n = n
+        self.be = be
 
     def features(self, string):
-        return self._each_cons(SENTINAL_CHAR + string + SENTINAL_CHAR, self.n)
+        if self.be:
+            string = (
+                SENTINAL_CHAR * (self.n - 1)
+                + string
+                + SENTINAL_CHAR * (self.n - 1)
+            )
+
+        features = self._each_cons(
+            SENTINAL_CHAR + string + SENTINAL_CHAR, self.n
+        )
+        feature_count = Counter(features)
+
+        final_features = set()
+        for k, v in feature_count.items():
+            if v > 1:
+                final_features.add("{}{}".format(k, v))
+            else:
+                final_features.add(k)
+
+        return final_features
