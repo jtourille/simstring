68e34836f350f18bfdeb0db0a49092dab1209a86
Julien Tourille <julien.tourille@gmail.com>
2021-02-09 14:27:30 +0100

    clean BaseDatabase

--- simstring/database/base.py
+++ simstring/database/base.py
@@ -1,15 +1,21 @@
-class BaseDatabase:
-    def __init__(self, feature_extractor):
-        raise 'Not Implemented'
+from simstring.feature_extractor.base import BaseFeatureExtractor
 
-    def add(self, string):
-        raise 'Not Implemented'
+
+class BaseDatabase:
+    def __init__(self, feature_extractor: BaseFeatureExtractor = None):
+        self.feature_extractor = feature_extractor
+        raise NotImplementedError
+
+    def add(self, string: str = None):
+        raise NotImplementedError
 
     def min_feature_size(self):
-        raise 'Not Implemented'
+        raise NotImplementedError
 
     def max_feature_size(self):
-        raise 'Not Implemented'
+        raise NotImplementedError
 
-    def lookup_strings_by_feature_set_size_and_feature(self, size, feature):
-        raise 'Not Implemented'
+    def lookup_strings_by_feature_set_size_and_feature(
+        self, size: int = None, feature: str = None
+    ):
+        raise NotImplementedError
