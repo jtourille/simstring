4d438ffa16c4865c087a57be160739c029b38b92
Julien Tourille <julien.tourille@gmail.com>
2021-02-09 14:28:22 +0100

    simplify DictDatabase: remove unnecessary functions and mappings

--- simstring/database/dict.py
+++ simstring/database/dict.py
@@ -1,33 +1,33 @@
 from collections import defaultdict
-from .base import BaseDatabase
 
-def defaultdict_set():
-    return defaultdict(set)
+from simstring.database.base import BaseDatabase
+
 
 class DictDatabase(BaseDatabase):
     def __init__(self, feature_extractor):
-        self.feature_extractor = feature_extractor
-        self.strings = []
-        self.feature_set_size_to_string_map = defaultdict(set)
-        self.feature_set_size_and_feature_to_string_map = defaultdict(defaultdict_set)
+        super().__init__(feature_extractor=feature_extractor)
 
-    def add(self, string):
+        self.feature_extractor = feature_extractor
+        self.feature_set_size_and_feature_to_string_map = defaultdict(
+            lambda: defaultdict(set)
+        )
+
+    def add(self, string: str = None):
         features = self.feature_extractor.features(string)
         size = len(features)
 
-        self.strings.append(string)
-        self.feature_set_size_to_string_map[size].add(string)
         for feature in features:
-            self.feature_set_size_and_feature_to_string_map[size][feature].add(string)
+            self.feature_set_size_and_feature_to_string_map[size][feature].add(
+                string
+            )
 
-    def all(self):
-        return self.strings
-
-    def lookup_strings_by_feature_set_size_and_feature(self, size, feature):
+    def lookup_strings_by_feature_set_size_and_feature(
+        self, size: int = None, feature: str = None
+    ):
         return self.feature_set_size_and_feature_to_string_map[size][feature]
 
     def min_feature_size(self):
-        return min(self.feature_set_size_to_string_map.keys())
+        return min(self.feature_set_size_and_feature_to_string_map.keys())
 
     def max_feature_size(self):
-        return max(self.feature_set_size_to_string_map.keys())
+        return max(self.feature_set_size_and_feature_to_string_map.keys())
